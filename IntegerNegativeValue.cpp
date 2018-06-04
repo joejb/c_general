9050bb7e97d1dbddb630cf027087bf5c38fd6f05
joejb <joebh@hotmail.com>
2018-06-04 12:45:12 -0400

    Create IntegerNegativeValue.cpp

---
+++ IntegerNegativeValue.cpp
@@ -0,0 +1,8 @@
+#include <limits.h>
+#include <stdio.h>
+
+void main(void) {
+  printf("The most negative value of type int \n");
+  printf("represented on this C system is ");
+  printf("%d\n", INT_MIN);
+}
