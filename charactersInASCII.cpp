4d017dc3e2b8da2e0b85127521d4fa8a5ea64b4f
joejb <joebh@hotmail.com>
2018-06-04 13:08:54 -0400

    Create charactersInASCII.cpp

---
+++ charactersInASCII.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(void)
+{
+  char sym_1, sym_2, sym_3;
+
+  sym_1 = 'a';
+  sym_2 = '2';
+  sym_3 = 'Z';
+
+  printf("sym_1 = %d in ASCII\n", sym_1);
+  printf("sym_2 = %d in ASCII\n", sym_2);
+  printf("sym_3 = %d in ASCII\n", sym_3);
+}
